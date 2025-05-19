d5d606ae90dcafc790fdaf1a5d652b5275fd55e2
Dutchmen04 <maxmaes2004@gmail.com>
2025-05-19 16:06:06 +0200

    fix: fixed naming thing

--- ORCID.Net/Services/IPersonRetrievalService.cs
+++ ORCID.Net/Services/IPersonRetrievalService.cs
@@ -9,7 +9,7 @@ namespace ORCID.Net.Services;
 
 public interface IPersonRetrievalService
 {
-    Task<Person> FindPersonByOrcid(string personId);
+    Task<Person> FindPersonByOrcid(string orcId);
     
     Task<List<Person>> FindPeopleByName(string personName, int resultAmount);
     
